ae3421021fa963b9a47903f1873872be43dc0656
Callum Ellis <Callum Ellis>
2016-03-27 22:21:55 +0100

    Initial Commit - Laravel setup with Auth generated and some initial design work

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Welcome</div>
+
+                <div class="panel-body">
+                    Module Tracker is a website designed to help you keep track of your modules in school or university. You can add your modules, assignments, predicted grades and current percentages and the website will work out what you need to get for each assignment and let you know if you're on track etc.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Inside Look</div>
+
+                <div class="panel-body">
+                    [Screenshot of the site when logged in will go here.]
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
