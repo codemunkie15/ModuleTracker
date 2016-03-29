441ce928038379db23d977d28b295942a489f5a6
Callum Ellis <Callum Ellis>
2016-03-29 19:36:06 +0100

    Added comments to controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,25 +10,26 @@ use App\Module;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
+     * Home controller constructor
+     * Forces authentication so you have to be logged in
      */
     public function __construct() {
         $this->middleware('auth');
     }
 
     /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
+     * Show the summary view
      */
     public function view_home($order_by = 'module_code') {
+        // Collect the modules from the database for the user
         $modules = Module::where('user_id', Auth::id())->orderBy($order_by, 'asc')->get();
         $assignments = array();
+        // Loop through each module, fetching the assignments for the module
         foreach($modules as $module) {
+            // Add the assignments to an arry to access later
             $assignments[] = Assignment::where('module_id', $module->id)->orderBy('assignment_name', 'asc')->get();
         }
+        // Return the view passing the data
         return view('home', [
             'modules' => $modules,
             'assignments' => $assignments,
--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -11,41 +11,66 @@ use Validator;
 
 class ModuleController extends Controller {
 
+    /**
+     * Module controller constructor
+     * Forces authentication so you have to be logged in
+     */
     public function __construct() {
         $this->middleware('auth');
     }
 
+    /**
+     * Show the add modules and assignments view
+     */
     public function view_add() {
+        // Get the modules for the user so we can display it on the select box (when adding an assignment)
         $modules = Module::where('user_id', Auth::id())->orderBy('module_code', 'asc')->get();
+        // Return the view with the data
         return view('add_modules', [
             'modules' => $modules
         ]);
     }
 
+    /**
+     * Show the edit modules and assignments view
+     */
     public function view_edit_mod() {
         return view('edit_modules');
     }
 
+    /**
+     * Add a new module - called from the post route (Form)
+     */
     public function add_new_module(Request $request) {
+        // Create a validator
         $validator = Validator::make($request->all(), [
             'module_code' => 'required|max:20',
             'module_name' => 'required|max:50'
         ]);
 
+        // Validate
         if($validator->fails()) {
+            // Redirect to module view and pass the errors
             return redirect()->back()->withErrors($validator, 'module')->withInput();
         }
 
+        // Create the module
         $module = new Module();
         $module->user_id = Auth::id();
         $module->module_code = $request['module_code'];
         $module->module_name = $request['module_name'];
 
+        // Save to database
         $module->save();
+        // Redirect to add module view and pass a success message
         return redirect()->back()->with('module_success_message', 'The module has successfully been added.');
     }
 
+    /**
+     * Add a new assignment - called from the post route (Form)
+     */
     public function add_new_assignment(Request $request) {
+        // Setup a validator (with a custom error message)
         $validator = Validator::make($request->all(), [
             'module_id' => 'required',
             'assignment_name' => 'required|max:80',
@@ -55,17 +80,21 @@ class ModuleController extends Controller {
             'module_id.required' => 'You need to choose a module for the assignment.'
         ]);
 
+        // Validate
         if($validator->fails()) {
             return redirect()->back()->withErrors($validator, 'assignment')->withInput();
         }
 
+        // Create assignment
         $assignment = new Assignment();
         $assignment->module_id = $request['module_id'];
         $assignment->assignment_name = $request['assignment_name'];
         $assignment->mark_percentage = $request['assignment_percentage'];
         $assignment->deadline = $request['assignment_deadline'];
 
+        // Save to database
         $assignment->save();
+        // Redirect to add module page and pass success message
         return redirect()->back()->with('assignment_success_message', 'The assignment has successfully been added.');
     }
 }
